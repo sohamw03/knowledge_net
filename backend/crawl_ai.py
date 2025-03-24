96356534fc64827552db5a282b53b4ef73a8f069
Soham Waghmare <102748742+sohamw03@users.noreply.github.com>
2025-03-25 01:55:28 +0530

    feat: concurrent ws conns, parallel scraping, better image extraction

--- backend/crawl_ai.py
+++ backend/crawl_ai.py
@@ -1,14 +1,10 @@
 import asyncio
 import json
-import sys 
 
 from crawl4ai import AsyncWebCrawler, BrowserConfig, CacheMode
 
 
-# from base64 import b64decode
-
-
-async def main():
+async def main(urls):
     base_browser = BrowserConfig(
         browser_type="chromium",
         headless=True,
@@ -20,18 +16,28 @@ async def main():
     # Create an instance of AsyncWebCrawler
     async with AsyncWebCrawler(config=base_browser) as crawler:
         # Run the crawler on a URL
-        result = await crawler.arun(url=sys.argv[1], screenshot=False, cache_mode=CacheMode.BYPASS)
-        # Print the extracted content
-        hr = lambda: print(("-" * 80) * 2)
-        hr()
-        print(result.markdown)
-        hr()
-        print(json.dumps(result.media, indent=2))
-        hr()
-        print(json.dumps(result.links, indent=2))
-        hr()
-        print(json.dumps(result.downloaded_files, indent=2))
-        hr()
+        results = await crawler.arun_many(
+            urls=urls,
+            screenshot=False,
+            cache_mode=CacheMode.BYPASS,
+            scan_full_page=True,
+            semaphore_count=3,
+            wait_for_images=True,
+        )
+        with open("output.json", "w") as f:
+            f.write("")
+        for result in results:
+            if result.success:
+                dump_result = {
+                    "url": result.url,
+                    "markdown": result.markdown,
+                }
+                with open("output.json", "a") as f:
+                    json.dump(dump_result, f)
+                # Print the extracted content
+                hr = lambda n=1: print(("-" * 80) * 2 * n)
+                print("[OK] URL:", result.url)
+                hr()
 
         # if result.success:
         #     # Save screenshot
@@ -50,4 +56,11 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    urls = [
+        "https://www.google.com",
+        "https://www.amazon.com",
+        "https://www.facebook.com",
+        "https://www.twitter.com",
+        "https://www.instagram.com",
+    ]
+    asyncio.run(main(urls))
